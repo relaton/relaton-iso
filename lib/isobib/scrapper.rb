1769333d5e0e59a7c758be4ba291b91e68cd28e5
Nick Nicholas <opoudjis@optusnet.com.au>
2018-08-25 01:41:05 +1000

    ISO docid prefix

--- lib/isobib/scrapper.rb
+++ lib/isobib/scrapper.rb
@@ -194,7 +194,7 @@ module Isobib
       # @return [Hash]
       def fetch_docid(doc)
         item_ref = doc.xpath("//strong[@id='itemReference']").text
-                      .match(/(?<=\s)(\d+)-?((?<=-)\d+|)/)
+          .match(/^(.*?\d+)-?((?<=-)\d+|)/)
         { project_number: item_ref[1], part_number: item_ref[2], prefix: nil }
       end
 
