ea079a7e7b978c2078988ddc430318173e008927
Artur Komarov <artur.komarov@gmail.com>
2022-05-20 16:19:36 +0700

    Refactor filtering hits by year

--- spec/relaton_iso/hit_spec.rb
+++ spec/relaton_iso/hit_spec.rb
@@ -10,4 +10,15 @@ RSpec.describe RelatonIso::Hit do
       expect(hit.sort_weight).to eq 4
     end
   end
+
+  describe "#pubid" do
+    subject { described_class.new({ title: title }).pubid }
+
+    let(:title) { "#{pubid} Geographic information — Metadata — Part 1: Fundamentals" }
+    let(:pubid) { "ISO 19115-1:2014" }
+
+    it "extracts pubid from title" do
+      expect(subject.to_s).to eq(pubid)
+    end
+  end
 end
