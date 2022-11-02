04aa4d7a01c075b8c31e357cd7b37495958bfa2b
Artur Komarov <artur.komarov@gmail.com>
2022-11-02 18:36:37 +0700

    Update edition for base identifier when available

--- lib/relaton_iso/hit.rb
+++ lib/relaton_iso/hit.rb
@@ -6,11 +6,26 @@ module RelatonIso
     # @return [RelatonIsoBib::IsoBibliographicItem]
     attr_writer :fetch, :pubid
 
+    # Update edition for pubid when provided in Bibliographic Item
+    def update_edition(bibliographic_item)
+      if bibliographic_item.edition
+        # add edition to base document if available
+        if pubid.base
+          pubid.base.edition = bibliographic_item.edition.content
+        else
+          pubid.edition = bibliographic_item.edition.content
+        end
+      end
+    end
+
     # Parse page.
     # @param lang [String, nil]
     # @return [RelatonIso::IsoBibliographicItem]
     def fetch(lang = nil)
       @fetch ||= Scrapper.parse_page self, lang
+      # update edition for pubid using fetched data
+      update_edition(@fetch)
+      @fetch
     end
 
     # @return [Integer]
