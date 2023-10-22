03cde052d360a1bebdebc9eaea96d349370a159f
Andrei Kislichenko <android.2net@gmail.com>
2023-10-22 00:24:31 -0400

    fix issue #157

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -4,7 +4,7 @@ VCR.configure do |config|
   config.cassette_library_dir = "spec/vcr_cassettes"
   config.default_cassette_options = {
     clean_outdated_http_interactions: true,
-    re_record_interval: 7 * 24 * 3600,
+    re_record_interval: 23 * 3600,
     record: :once,
   }
   config.hook_into :webmock
