267849548c036901751f3e9df51f009528c42f69
Andrei Kislichenko <android.2net@gmail.com>
2026-02-06 00:17:48 -0500

    Update VCR cassettes and remove deep_clone method

--- lib/relaton/iso/item_data.rb
+++ lib/relaton/iso/item_data.rb
@@ -1,10 +1,6 @@
 module Relaton
   module Iso
     class ItemData < Bib::ItemData
-      def deep_clone
-        Item.from_yaml Item.to_yaml(self)
-      end
-
       def create_id(without_date: false)
         docid = docidentifier.find(&:primary) || docidentifier.first
         return unless docid
